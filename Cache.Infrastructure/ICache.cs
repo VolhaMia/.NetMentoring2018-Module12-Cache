3b8848a5dcd85d96b8a4c3635751def23e9d4ba5
Olga <volha_miashkova@epam.com>
2018-12-10 18:14:34 +0300

    add files

---
+++ Cache.Infrastructure/ICache.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Cache.Infrastructure
+{
+    public interface ICache<T>
+    {
+        T Get(string key);
+        void Set(string key, T value, DateTimeOffset expirationDate);
+    }
+}
